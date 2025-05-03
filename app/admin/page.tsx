3d9823ec8a76fd5ae0ea29ef07ebfee282307f34
Anilton Teixeira <aniltontlima@gmail.com>
2025-05-03 13:25:14 -0300

    pagina admin

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { User } from "@/modules/user/user-repository";
+
+export default function UsuariosPage() {
+
+
+  return (
+    <h1>Area administrativa</h1>
+  );
+}
