88dfa937989e55b5ab182e3802267b0b0bc96fcf
Anilton Teixeira <aniltontlima@gmail.com>
2025-05-03 13:24:42 -0300

    criei a page do dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { User } from "@/modules/user/user-repository";
+
+export default function DashboardPage() {
+
+
+  return (
+    <h1>Dashboard</h1>
+  );
+}
